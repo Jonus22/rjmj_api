78cd1ece8bf7b622adc0344333072135e74ddbc1
Jonus Berdon <jbprog22@gmail.com>
2024-08-10 20:30:05 +0800

    test commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type', 50)->nullable(false)->default('deault');
+            $table->integer('flag')->nullable(false)->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
